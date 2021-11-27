b3f4b489dbf519d5287ec5445b09dec7617b8cfd
Janek Szynal <jan.szynal@gmail.com>
2021-11-27 13:18:35 +0100

    Clean up a bit, change the flow for webdriver var

--- bouldergarten.py
+++ bouldergarten.py
@@ -2,9 +2,27 @@ import time
 from selenium.webdriver.common.by import By
 from selenium_helpers import get_driver, process_dates
 
-driver = get_driver()
+
+# TODO: Move to selenium_helpers, maybe also rename that file
+def check():
+  driver = get_driver()
+  open_bookings(driver)
+  # XXX: We might have to exclude the dates with the `drp-date-not-relevant` class, unless date is pre-set
+  # Selenium has a not, but unclear how to apply it to the class of the containing elements.
+  # https://www.qafox.com/selenium-locators-using-not-in-css-selectors/ (too long article)
+  # items = driver.find_elements_by_css_selector("div.examplenameA:not(.examplenameB)")
+  time.sleep(0.5)
+  element = driver.find_element(By.CSS_SELECTOR, ".drp-course-dates-list-wrap")
+  dates = process_dates(element.get_attribute('innerHTML'))
+  return dates
+
+def book(user):
+  if user == "rrszynka":
+    open_bookings(driver)
+  return "Coming soon!"
 
 def open_bookings(driver, for_real=False):
+  driver = get_driver()
   driver.get("https://bouldergarten.de/")
   # XXX: JS "clicks" vs "mouse" clicks()
   # 1. the issue that forced our usage of JS click might be resolved by waits)
@@ -31,20 +49,3 @@ def open_bookings(driver, for_real=False):
 
   time.sleep(1)
   element = driver.find_element(By.CSS_SELECTOR, ".drp-calendar-day-dates").click()
-
-# TODO: Move to selenium_helpers, maybe also rename that file
-def check():
-  open_bookings(driver)
-  # XXX: We might have to exclude the dates with the `drp-date-not-relevant` class, unless date is pre-set
-  # Selenium has a not, but unclear how to apply it to the class of the containing elements.
-  # https://www.qafox.com/selenium-locators-using-not-in-css-selectors/ (too long article)
-  # items = driver.find_elements_by_css_selector("div.examplenameA:not(.examplenameB)")
-  time.sleep(0.5)
-  element = driver.find_element(By.CSS_SELECTOR, ".drp-course-dates-list-wrap")
-  dates = process_dates(element.get_attribute('innerHTML'))
-  return dates
-
-def book(user):
-  if user == "rrszynka":
-    open_bookings(driver)
-  return "Coming soon!"
--- boulderklub.py
+++ boulderklub.py
@@ -1,17 +1,18 @@
-from selenium.webdriver.common.by import By
-from selenium_helpers import get_driver, process_dates
-
-driver = get_driver()
-
-def open_bookings():
-  driver.get("https://boulderklub.de/")
-  element = driver.find_element(By.CSS_SELECTOR, ".drp-calendar-day-dates").click()
+import time
+from selenium_helpers import get_driver, process_date
 
 def check():
+  driver = get_driver()
   open_bookings()
+  time.sleep(0.5)
   element = driver.find_element(By.CSS_SELECTOR, ".drp-course-dates-list-wrap")
   dates = process_dates(element.get_attribute('innerHTML'))
   return dates
 
 def book(user):
   return "Coming soon!"
+
+def open_bookings():
+  driver = get_driver()
+  driver.get("https://boulderklub.de/")
+  element = driver.find_element(By.CSS_SELECTOR, ".drp-calendar-day-dates").click()
