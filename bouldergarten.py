c046fd39b34b36e2945c581d0e59c1ccd2c0228e
Janek Szynal <jan.szynal@gmail.com>
2021-11-27 13:53:42 +0100

    Add timing to tests

--- bouldergarten.py
+++ bouldergarten.py
@@ -1,10 +1,17 @@
 import time
+import logging
 from selenium.webdriver.common.by import By
 from selenium_helpers import get_driver, process_dates
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 # TODO: Move to selenium_helpers, maybe also rename that file
 def check():
+  start_time = time.time()
   driver = get_driver()
   open_bookings(driver)
   # XXX: We might have to exclude the dates with the `drp-date-not-relevant` class, unless date is pre-set
@@ -14,6 +21,8 @@ def check():
   time.sleep(0.5)
   element = driver.find_element(By.CSS_SELECTOR, ".drp-course-dates-list-wrap")
   dates = process_dates(element.get_attribute('innerHTML'))
+  end_time = time.time()
+  logger.info(f"Checked Bouldergarten in {round(end_time - start_time, 2)}s")
   return dates
 
 def book(user):
--- boulderklub.py
+++ boulderklub.py
@@ -1,12 +1,22 @@
 import time
+import logging
+from selenium.webdriver.common.by import By
 from selenium_helpers import get_driver, process_dates
 
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 def check():
+  start_time = time.time()
   driver = get_driver()
   open_bookings()
   time.sleep(0.5)
   element = driver.find_element(By.CSS_SELECTOR, ".drp-course-dates-list-wrap")
   dates = process_dates(element.get_attribute('innerHTML'))
+  end_time = time.time()
+  logger.info(f"Checked Boulderklub in {round(end_time - start_time, 2)}s")
   return dates
 
 def book(user):
