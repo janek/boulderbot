48a2705b31809a9f9a3439baca14d8d02f0b72f7
Janek Szynal <jan.szynal@gmail.com>
2022-01-24 18:46:41 +0100

    Refactor tests

---
+++ test_checking_gyms.py
@@ -0,0 +1,17 @@
+from boulderbot import check_gym
+
+def test_bouldergarten():
+    check_gym("Bouldergarten")
+    return True
+
+def test_boulderklub():
+    check_gym("Boulderklub")
+    return True
+
+def test_suedbloc():
+    check_gym("Suedbloc")
+    return True
+
+def test_der_kegel():
+    check_gym("Der Kegel")
+    return True
