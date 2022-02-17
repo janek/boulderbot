9be470836e3c85ab7337d29f3718904f2942e815
Janek Szynal <jan.szynal@gmail.com>
2022-02-17 21:30:05 +0100

    Set up svelte test project, implement loading info from a json file

---
+++ svelte-app/src/routes/index.svelte
@@ -0,0 +1,30 @@
+<script>
+	import * as jsonInfo from '/Users/janek/Developer/boulderbot/cache/Boulderklub.json';
+	console.log(jsonInfo);
+
+	function jsonGymInfoToText(info) {
+		return info
+			.filter((slotInformation) => slotInformation.free_places != 0)
+			.reduce(
+				(outputHTML, slotInformation) =>
+					outputHTML +
+					slotInformation.start_time +
+					' - ' +
+					slotInformation.end_time +
+					' → ' +
+					slotInformation.free_places +
+					' places ' +
+					'<br/>',
+				''
+			);
+	}
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>Boulderklub</h1>
+	<div>{@html jsonGymInfoToText(jsonInfo.default)}</div>
+</section>
