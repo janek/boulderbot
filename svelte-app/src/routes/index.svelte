e7da440f253b4568e3294d17cfa527f0239eca3a
Janek Szynal <jan.szynal@gmail.com>
2022-02-19 14:03:09 +0100

    Display hall info as text

--- svelte-app/src/routes/index.svelte
+++ svelte-app/src/routes/index.svelte
@@ -1,22 +1,40 @@
 <script>
-	import * as jsonInfo from '/Users/janek/Developer/boulderbot/cache/Boulderklub.json';
-	console.log(jsonInfo);
+	const halls = ['Bouldergarten', 'Boulderklub', 'Der Kegel', 'Suedbloc'];
+	// TODO: enable TS and define JSON schema?
+	import * as jsonInfo from '/Users/janek/Developer/boulderbot/cache/all.json';
+	const today = new Date().toISOString().slice(0, 10);
+	console.log(jsonInfo.default[halls[2]][today]);
 
-	function jsonGymInfoToText(info) {
-		return info
-			.filter((slotInformation) => slotInformation.free_places != 0)
-			.reduce(
-				(outputHTML, slotInformation) =>
-					outputHTML +
-					slotInformation.start_time +
-					' - ' +
-					slotInformation.end_time +
-					' → ' +
-					slotInformation.free_places +
-					' places ' +
-					'<br/>',
-				''
-			);
+	console.log(hallInfoForToday(halls[2]));
+
+	function hallInfoForToday(hall) {
+		const today = new Date().toISOString().slice(0, 10);
+		return hallInfoPerDay(hall, today);
+	}
+
+	function hallInfoPerDay(hall, day) {
+		console.log(`Getting info for ${hall}, ${day}`);
+		const infoPerDay = jsonInfo.default[hall][day];
+		console.log(infoPerDay);
+		console.log(typeof infoPerDay);
+		console.log(infoPerDay.length);
+		console.log('\n');
+		if (infoPerDay.length === 0) {
+			return 'No slots available';
+		}
+		// 'day' formatted as 2022-02-18
+		return infoPerDay.reduce(
+			(outputHTML, slotInformation) =>
+				outputHTML +
+				slotInformation.start_time +
+				' - ' +
+				slotInformation.end_time +
+				' → ' +
+				String(slotInformation.free_places).padStart(2, '\u00A0') +
+				' places ' +
+				'<br/>',
+			''
+		);
 	}
 </script>
 
@@ -25,6 +43,8 @@
 </svelte:head>
 
 <section>
-	<h1>Boulderklub</h1>
-	<div>{@html jsonGymInfoToText(jsonInfo.default)}</div>
+	{#each halls as hall}
+		<h1>{hall}</h1>
+		<p>{@html hallInfoForToday(hall)}</p>
+	{/each}
 </section>
