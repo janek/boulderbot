10134c25549c641ffec2972898e6ac8d0997e772
Janek Szynal <jan.szynal@gmail.com>
2022-03-06 15:41:48 +0100

    turn off webhooks

--- boulderbot.py
+++ boulderbot.py
@@ -115,12 +115,12 @@ def main():
 
     # Start the Bot
     # if not program_is_running_on_heroku: # TODO: this condition is broken, debug maybe by printing os.environ
-    webhooks = False
+    webhooks = True
     if webhooks:
         updater.start_webhook(listen="0.0.0.0",
                             port=int(PORT),
-                            url_path=TOKEN)
-                            # webhook_url="https://ricchardo-bukowski.herokuapp.com/" + TOKEN)
+                            url_path=TOKEN,
+                            webhook_url="https://ricchardo-bukowski.herokuapp.com/" + TOKEN)
         logger.info('Running with webhooks')
     else:
         updater.start_polling()
