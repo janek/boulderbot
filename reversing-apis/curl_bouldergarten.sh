a4398362c49a40821b8b1b18b8ae604aa369ff73
Janek Szynal <jan.szynal@gmail.com>
2022-03-11 20:41:52 +0100

    json example

---
+++ reversing-apis/curl_bouldergarten.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+curl 'https://backend.dr-plano.com/courses_dates?id=67359814&advanceToFirstMonthWithDates&start=1646089200000&end=1648764000000' \
+    -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:98.0) Gecko/20100101 Firefox/98.0' \
+    -H 'Accept: */*' \
+    -H 'Accept-Language: en-US,en;q=0.5' \
+    -H 'Origin: https://bouldergarten.de' \
+    -H 'Connection: keep-alive' \
+    -H 'Referer: https://bouldergarten.de/' \
+    -H 'Sec-Fetch-Dest: empty' \
+    -H 'Sec-Fetch-Mode: cors' \
+    -H 'Sec-Fetch-Site: cross-site' -H 'TE: trailers'
