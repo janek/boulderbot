112955b943395e84d626e86e42bf8259aab924dd
Janek Szynal <jan.szynal@gmail.com>
2022-04-03 16:40:01 +0200

    Add some basic safety checks

--- webapp/src/routes/index.svelte
+++ webapp/src/routes/index.svelte
@@ -2,18 +2,22 @@
 	import * as jsonInfo from '/Users/janek/Developer/boulderbot/cache/all.json';
 	const halls = ['Bouldergarten', 'Boulderklub', 'Der Kegel', 'Suedbloc'];
 
-    let info = jsonInfo
+    let info = jsonInfo.default
 
 	function hallInfoForToday(hall) {
-		const today = new Date().toISOString().slice(0, 10);
+        const today = new Date().toISOString().slice(0, 10);
 		return hallInfoPerDay(hall, today);
 	}
 
 	function hallInfoPerDay(hall, day) {
 		/* Convert JSON to presentable html-ish text.
   		 'Day' is formatted as 2022-02-18 */
-		const infoPerDay = info.default[hall][day];
-		if (infoPerDay.length === 0) {
+        if (!(hall in info)) {
+            return "No info for " + hall;
+        }
+
+        const infoPerDay = info[hall][day];
+		if (!infoPerDay || infoPerDay.length === 0) {
 			return 'No slots available';
 		}
 
@@ -34,12 +38,12 @@
 	}
 
     function refresh() {
-        const fetchJSON = fetch('http://localhost:8001/all.json')
-             .then((res) => res.json())
-             .then((data) => {
-                info = data;
-                console.log(info[halls[2]]["2022-02-19"][0]["start_time"]);
-             });
+        // const fetchJSON = fetch('http://localhost:8001/all.json')
+        //      .then((res) => res.json())
+        //      .then((data) => {
+        //         info = data;
+        //         console.log(info[halls[2]]["2022-02-19"][0]["start_time"]);
+        //      });
     }
 </script>
 
